c00f4331cf023e535f1d1df5121f512d4228fa37
cliffpm <cliff.pham256@gmail.com>
2026-04-09 00:35:35 -0700

    prepped viterbi algo.

--- wifi PHY/testing.py
+++ wifi PHY/testing.py
@@ -1,20 +1,57 @@
 import numpy as np
+import commpy as comm
+import commpy.modulation as modul
+import commpy.channelcoding.convcode as check
 
-msg = "hello"
+msg = np.array([0,0,0, 1,1,1,1,0])
 
-res = np.unpackbits(np.array([ord(c) for c in msg], dtype = np.uint8))
+mod = modul.QAMModem(4)
+output = mod.modulate(msg.astype(bool))
+# print(output)
 
-# print(res)
+cc1 = check.Trellis(np.array([3]),np.array([[0o7,0o5]])) # the encoder
 
-print(f"ord of 1 :", ord("1"))
-print(f"ord of 0 :", ord("0"))
+# print(cc1.next_state_table)
 
-res = [1,2,3,4]
+# print()
+# print(cc1.output_table)
+bits_to_ideal_complex = {0: -1-1j,
+                            1: -1 +1j,
+                            2: 1-1j,
+                            3: 1 + 1j}
 
-# print(res[1:])
+transition_table = {}
+for i in range(len(cc1.output_table)):
+    for j in range(len(cc1.output_table[0])):
+        # i represents the current state
+        # j represents the input
+        # arr[i][j] represents the next state
+        current_state = i
+        input = j
+        next_state = cc1.next_state_table[i][j]
+        output = cc1.output_table[i][j] #in decimal
+        transition_table[(current_state, input)] = [next_state, bits_to_ideal_complex[output]]
 
-print(np.unpackbits(np.array([49,48,49],dtype=np.uint8)))
+        cc1.num
 
-x =[0, 0, 1, 1,0, 0, 0, 1 ,0, 0, 1, 1, 0, 0, 0 ,0, 0 ,0, 1, 1, 0, 0, 0, 1]
+print(transition_table
+)
 
-print(len(x))
+# msg = "hello"
+
+# res = np.unpackbits(np.array([ord(c) for c in msg], dtype = np.uint8))
+
+# # print(res)
+
+# print(f"ord of 1 :", ord("1"))
+# print(f"ord of 0 :", ord("0"))
+
+# res = [1,2,3,4]
+
+# # print(res[1:])
+
+# print(np.unpackbits(np.array([49,48,49],dtype=np.uint8)))
+
+# x =[0, 0, 1, 1,0, 0, 0, 1 ,0, 0, 1, 1, 0, 0, 0 ,0, 0 ,0, 1, 1, 0, 0, 0, 1]
+
+# print(len(x))
--- wifi PHY/wifireceiver_template.py
+++ wifi PHY/wifireceiver_template.py
@@ -7,7 +7,76 @@ import commpy.channelcoding.convcode as check
 from pip import main
 import matplotlib.pyplot as plt
   
- 
+
+def SoftViterbiDecoder(input_stream, cc1):
+    
+
+
+
+    # use euclidean distance (squared dist.)
+    # ideal points : 
+    # 00 : -1 - 1j
+    # 01 : -1 + 1j
+    # 11 : 1  + 1j
+    # 10 : 1 -  1j
+    #
+    #
+    bits_to_ideal_complex = {0: -1-1j,
+                             1: -1 +1j,
+                             2: 1-1j,
+                             3: 1 + 1j}
+    
+
+
+    
+    # create a transition table 
+    # where (current_state, input_bit) -> [next_state, ideal_complex point]
+    transition_table = {}
+    for i in range(len(cc1.output_table)):
+        for j in range(len(cc1.output_table[0])):
+            # i represents the current state
+            # j represents the input
+            # arr[i][j] represents the next state
+            current_state = i
+            input = j
+            next_state = cc1.next_state_table[i][j]
+            output = cc1.output_table[i][j] #in decimal
+            transition_table[(current_state, input)] = [next_state, bits_to_ideal_complex[output]]
+
+    # start in state 0 ??
+    path_metric = [0]*cc1.number_states # keep track of total accumalted distance
+    
+    # each val in path_metric represents the 
+    # sum of all euclidean distances of best possible path
+    # from start of message to specific state i
+    for i in range(1, cc1.number_states):
+        path_metric[i] = float('inf')
+
+    
+    
+
+    
+    # row represents time
+    # column represents the 8 states
+
+    '''
+    at each step every state has two incoming edges
+    aka two candidates . compare total score of both paths
+    and pick the path with lower score and store
+    the index of the previous state in this grid
+
+    ex : best way to get to state 2 at time step 5 was coming
+    from state 4, then trellis_memory[5][2] = 4
+
+    '''
+    # dimension is len(num complex symbols) by num_states (8)
+    trellis_memory = [[0 for _ in range(cc1.number_states)] for _ in range(len(input_stream))] # 2d array of size num_symbols, 8
+    
+    # forward pass
+    for complex_symbol in input_stream:
+        
+
+
 
 def WifiReceiver(input_stream, level):
 
@@ -20,7 +89,6 @@ def WifiReceiver(input_stream, level):
     begin_zero_padding = 0
     message=""
     length=0
-
     if level >= 4:
         #Input QAM modulated + Encoded Bits + OFDM Symbols in a long stream
         #Output Detected Packet set of symbols
@@ -34,6 +102,18 @@ def WifiReceiver(input_stream, level):
     if level >= 2:
         #Input QAM modulated + Encoded Bits (ECC)
         #Output Interleaved bits + Encoded Length (exactly the input for level 1)
+
+        # 4 QAM means each complex number represents 2 bits.
+        # so input_stream[:len(preamble)//2] is the preamble . we should split it
+        stripped_preamble = input_stream[:len(preamble)//2]
+        # then at this point the encoded msg. length header is the first 128 bits
+        length = stripped_preamble[:nfft] # DONT FORGET THAT EACH ITEM IN STREAM IS 2 BITS
+        data_block = stripped_preamble[nfft:]
+        # now since I want to implement soft viterbi decoding
+        # i will use the 
+        #demodulated +decoded trellis stream
+        decrypt = SoftViterbiDecoder(data_block)
+
         input_stream=input_stream
        
     if level >= 1: # input is bitstream 1D array of floats
